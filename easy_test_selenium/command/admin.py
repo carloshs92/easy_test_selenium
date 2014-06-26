0f53ad3bb6224de026672bf8221594ca045334a3
Carlos Huamani <carlos.huamani@orbis.pe>
2014-06-26 16:06:31 -0500

    Command Line Tools: create connection

---
+++ easy_test_selenium/command/admin.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+print 'yeaaaaaaaaaaah aqui se contruye el proyecto'
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(
   name = 'easy_test_selenium',
-  packages = ['easy_test_selenium'], # this must be the same as the name above
+  packages = ['easy_test_selenium', 'easy_test_selenium.command'], # this must be the same as the name above
   version = '0.0.1',
-  description = 'An app to create data to test',
+  description = 'A python mini framework for tests that use Selenium',
+  scripts=['easy_test_selenium/command/admin.py'],
   author = 'Carlos Huamani',
   author_email = 'carlos.hs.92@gmail.com',
   url = 'https://github.com/carloshs92/easy_test_selenium', # use the URL to the github repo
