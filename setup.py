6974fbfd0d7b8dff745a25f1941d0d25c0544e0b
Carlos Huamani <carlos.huamani@orbis.pe>
2014-06-26 09:20:13 -0500

    Estructura del paquete easy_test

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'esasy_test_selenium',
+  packages = ['easy_test_selenium'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'An app to create data to test',
+  author = 'Carlos Huamani',
+  author_email = 'carlos.hs.92@gmail.com',
+  url = 'https://github.com/carloshs92/easy_test_selenium', # use the URL to the github repo
+  download_url = 'https://github.com/carloshs92/easy_test_selenium/archive/master.zip', # I'll explain this in a second
+  keywords = ['test', 'functional test', 'selenium'], # arbitrary keywords
+  classifiers = [],
+)
+
