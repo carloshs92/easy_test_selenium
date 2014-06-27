8319a11afb54f012b22587217e3256541c68b486
Carlos Huamani <carlos.huamani@orbis.pe>
2014-06-27 12:17:27 -0500

    Nueva estructura

--- command/admin.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-print 'yeaaaaaaaaaaah aqui se contruye el proyecto'
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 setup(
-  name = 'easy_test_selenium',
-  packages = ['easy_test_selenium', 'command'], # this must be the same as the name above
-  version = '0.0.1',
-  include_package_data=True,
-  description = 'A python mini framework for tests that use Selenium',
-  scripts=['command/admin.py'],
-  author = 'Carlos Huamani',
-  author_email = 'carlos.hs.92@gmail.com',
-  url = 'https://github.com/carloshs92/easy_test_selenium', # use the URL to the github repo
-  download_url = 'https://github.com/carloshs92/easy_test_selenium/archive/master.zip', # I'll explain this in a second
-  keywords = ['test', 'functional test', 'selenium'], # arbitrary keywords
-  classifiers = [],
+    name='easyTestSelenium',
+    packages=['easy_test_selenium', 'commands'],
+    version='0.0.1',
+    include_package_data=True,
+    description='A python mini framework for tests that use Selenium',
+    scripts=['commands/admin.py'],
+    author='Carlos Huamani',
+    author_email='carlos.hs.92@gmail.com',
+    url='https://github.com/carloshs92/easy_test_selenium',
+    download_url='https://github.com/carloshs92/easy_test_selenium/archive/master.zip',
+    keywords=['test', 'functional test', 'selenium'],
+    classifiers=[],
 )
 
